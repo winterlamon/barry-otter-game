d3967b420f4771583d435328f626d662faa12230
github username <github email address>
2017-12-04 12:55:48 -0500

    seeded character database

--- back-end/db/seeds.rb
+++ back-end/db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Character.create(name: 'Harry Potter', house: 'Gryffindor', health: 100)
+Character.create(name: 'Cedric Diggory', house: 'Hufflepuff', health: 100)
+Character.create(name: 'Luna Lovegood', house: 'Ravenclaw', health: 100)
+Character.create(name: 'Hermione Granger', house: 'Gryffindor', health: 100)
+Character.create(name: 'Ron Weasley', house: 'Gryffindor', health: 100)
+Character.create(name: 'Albus Dumbledore', house: 'Gryffindor', health: 100)
+Character.create(name: 'Draco Malfoy', house: 'Slytherin', health: 100)
+Character.create(name: 'Lord Voldemort', house: 'Slytherin', health: 100)
+Character.create(name: 'Severus Snape', house: 'Slytherin', health: 100)
+Character.create(name: 'Dolores Umbridge', house: 'Slytherin', health: 100)
