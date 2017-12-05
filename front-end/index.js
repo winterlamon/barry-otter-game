d4a6b4442802ee234c25dc6ac81849324937eed6
github username <github email address>
2017-12-05 16:57:24 -0500

    added event listeners to actions

--- front-end/index.js
+++ front-end/index.js
@@ -61,7 +61,7 @@ function characterProfile(player) {
   charProfileDiv.appendChild(actionUL)
 
   let spellTypes = ["Attack", "Defend", "Heal"]
-  for (type of spellTypes) {
+  for (let type of spellTypes) {
     // creates <li>
     let buttonLi = document.createElement('li');
     actionUL.appendChild(buttonLi);
@@ -71,32 +71,26 @@ function characterProfile(player) {
     buttonLi.append(typeButton);
     typeButton.innerText = type;
 
-    // typeButton.addEventListener('click', (event) => {
-    //   debugger;
-    //   let spellList = player.spells.filter(spell => spell.category === type.toLowerCase());
-    //   let spellUL = document.createElement('ul')
-    //   console.log(player)
-    //
-    // })
+    typeButton.addEventListener('click', (event) => {
+      // debugger;
+      let spellList = player.spells.filter(spell => (spell.category === type.toLowerCase()));
+      let spellUL = document.createElement('ul');
+      console.log(spellList);
+    })
   }
 
-  // spellTable.innerHTML =
-  //         `<tr> <td> <button category="attack">Attack</button>Attack </td> </tr>
-  //         <tr> <td> Heal </td> </tr>
-  //         <tr> <td> Defend </td> </tr>`
-  // charProfileDiv.appendChild(spellTable)
+  player.div.appendChild(charProfileDiv)
 
-
-  charProfileDiv.innerHTML +=
-    `<img class="character-image" src="${player.imageUrl}" alt="">
+  let imageDiv = document.createElement('div')
+  imageDiv.innerHTML +=
+    `<img class="character-image" src="${player.imageUrl}" alt=""></img>
       <div class="hc 1">
         <div class="hb 1">
           <center>${player.health} HP</center>
         </div>
-      </div>
-    </div>`
+      </div>`
 
-   player.div.appendChild(charProfileDiv)
+    charProfileDiv.appendChild(imageDiv)
 }
 
 // "id": 1,
