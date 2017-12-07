59fa9e682af7007cc0df4f5faa17764190810ada
MatteBru <mbruni616@gmail.com>
2017-12-07 16:15:12 -0500

    pulled mute listener out of startup() and put it in startMute()

--- front-end/test-index.js
+++ front-end/test-index.js
@@ -3,79 +3,15 @@ let player1
 let player2
 let game
 
-
-// class Character {
-//   constructor(id, name, house, health, spells, imageUrl) {
-//     this.id = id;
-//     this.name = name;
-//     this.house = house;
-//     this.health = health;
-//     this.spells = spells;
-//     this.imageUrl = imageUrl;
-//     this.moves = [];
-//   }
-//
-//   static all(){
-//     return store.characters;
-//   }
-//
-//   makeMove(spell){
-//     this.moves.push(spell)
-//   }
-//
-//   lastSpell(){
-//     return this.moves[this.moves.length - 1]
-//   }
-//
-//   prevMove(){
-//     // let move = this.moves[this.moves.length - 1];
-//     let move = this.lastSpell();
-//     let moveObj = {}
-//     moveObj[move['category']] = move['strength']
-//     return  moveObj;
-//   }
-//
-//   getMove(){
-//     let generic = {'attack': 0, 'defend':0, 'heal':0}
-//     let newMove = this.prevMove()
-//     let move = Object.assign(generic, newMove)
-//     return move;
-//   }
-//
-//   isAlive(){
-//     return this.health > 0;
-//   }
-//
-//   heal(strength){
-//     this.health += strength
-//     if (this.health > 100) {
-//       this.health = 100
-//     }
-//   }
-//
-//   damage(strength){
-//     this.health -= strength
-//     if (this.health < 0) {
-//       this.health = 0
-//     }
-//     let pb = this.div.querySelector('.progress-bar')
-//     pb.innerText = `${this.health} HP`
-//     pb.style.width = `${this.health}%`
-//   }
-//
-//   print(thing){
-//     this.gameDiv.innerText = thing;
-//   }
-//
-//   printText(thing){
-//     this.gameDiv.nextElementSibling.innerText = thing;
-//   }
-// }
-
-
 document.addEventListener('DOMContentLoaded', startup)
 
 function startup() {
+  console.log('hi')
+  startMute()
+  fetch('http://localhost:3000/characters').then(res => res.json()).then(json => createCharacters(json))
+}
+
+function startMute() {
   document.getElementById('mute').addEventListener('click', () => {
     if (!document.querySelector('audio').muted) {
       document.querySelector('audio').muted = true;
@@ -84,13 +20,7 @@ function startup() {
       document.querySelector('audio').muted = false;
       document.querySelector('#mute').innerHTML = '<i class="fa fa-volume-up"></i>'
     }
-
   })
-
-
-  console.log('hi')
-
-  fetch('http://localhost:3000/characters').then(res => res.json()).then(json => createCharacters(json))
 }
 
 function createCharacters(json) {
