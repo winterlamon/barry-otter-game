82a5469a9a1c1010003305cf34d68ab207bfc02b
MatteBru <mbruni616@gmail.com>
2017-12-07 16:19:29 -0500

    refactored mute to make it much prettier

--- front-end/test-index.js
+++ front-end/test-index.js
@@ -12,13 +12,15 @@ function startup() {
 }
 
 function startMute() {
-  document.getElementById('mute').addEventListener('click', () => {
-    if (!document.querySelector('audio').muted) {
-      document.querySelector('audio').muted = true;
-      document.querySelector('#mute').innerHTML = '<i class="fa fa-volume-off"></i>'
+  let mute = document.getElementById('mute')
+  let audio = document.querySelector('audio')
+  mute.addEventListener('click', () => {
+    if (!audio.muted) {
+      audio.muted = true;
+      mute.innerHTML = '<i class="fa fa-volume-off"></i>'
     }else {
-      document.querySelector('audio').muted = false;
-      document.querySelector('#mute').innerHTML = '<i class="fa fa-volume-up"></i>'
+      audio.muted = false;
+      mute.innerHTML = '<i class="fa fa-volume-up"></i>'
     }
   })
 }
